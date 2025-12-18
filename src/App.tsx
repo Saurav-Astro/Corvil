feb29c1318fa120eb2c04c90a40e1969c5dabe09
Coreveil Dev <dev@coreveil.local>
2025-12-19 04:16:44 +0530

    Add scroll-to-top on route change

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import GlobalCoverBackground from "@/components/GlobalCoverBackground";
+import ScrollToTop from "@/components/ScrollToTop";
 import Index from "./pages/Index";
 import Services from "./pages/Services";
 import About from "./pages/About";
@@ -35,6 +36,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter future={{ v7_relativeSplatPath: true }}>
+        <ScrollToTop />
         <GlobalCoverBackground />
         <AnimatedRoutes />
       </BrowserRouter>
